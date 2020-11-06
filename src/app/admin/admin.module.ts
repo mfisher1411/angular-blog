441b7bde1fdfa1da06a55302fdf1f77c1be2eb44
mfisher <matthewfisher1411@gmail.com>
2020-11-06 14:39:54 +0500

    added preloadingStrategy and all components in admin layout

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -1,6 +1,11 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
+import { AdminLayoutComponent } from './shared/admin-layout/admin-layout.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
+import { CreatePageComponent } from './create-page/create-page.component';
+import { EditPageComponent } from './edit-page/edit-page.component';
 
 @NgModule({
   imports: [
@@ -9,6 +14,7 @@ import {RouterModule} from '@angular/router';
   ],
   exports: [
     RouterModule
-  ]
+  ],
+  declarations: [AdminLayoutComponent, LoginPageComponent, DashboardPageComponent, CreatePageComponent, EditPageComponent]
 })
 export class AdminModule {}
