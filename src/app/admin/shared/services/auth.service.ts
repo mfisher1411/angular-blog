09db77c0b1dc33987008e41cd27c125b9b55a103
mfisher <matthewfisher1411@gmail.com>
2020-11-08 21:10:42 +0500

    added FbAuthResponse interface

--- src/app/admin/shared/services/auth.service.ts
+++ src/app/admin/shared/services/auth.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from '@angular/common/http';
 import {User} from '../../../shared/interfaces';
 import {Observable} from 'rxjs';
 import {environment} from '../../../../environments/environment';
+import {tap} from 'rxjs/operators';
 
 @Injectable()
 export class AuthService {
@@ -14,7 +15,11 @@ export class AuthService {
     return '';
   }
   login(user: User): Observable<any> {
-    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user);
+    user.returnSecureToken = true;
+    return this.http.post(`https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=${environment.apiKey}`, user)
+      .pipe(
+        tap(this.setToken)
+      );
   }
   logout(): void {}
   isAuthenticated(): boolean {
