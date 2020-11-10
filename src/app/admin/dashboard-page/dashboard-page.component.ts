d43b02c311e04830393a6ef079ae77182e071362
mfisher <matthewfisher1411@gmail.com>
2020-11-10 08:58:36 +0500

    finished the markup of the dashboard-page component

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,22 +1,31 @@
-import { Component, OnInit } from '@angular/core';
-import {AuthService} from '../shared/services/auth.service';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Post} from '../../shared/interfaces';
+import {PostsService} from '../../shared/posts.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
+  pSub: Subscription;
   constructor(
-    private auth: AuthService
+    private postsService: PostsService
   ) { }
 
   ngOnInit(): void {
+    this.pSub = this.postsService.getAll().subscribe((posts: Post[]) => {
+      this.posts = posts;
+    });
+  }
+  ngOnDestroy(): void {
+    if (this.pSub) {
+      this.pSub.unsubscribe();
+    }
   }
 
-  test(): void {
-    console.log(this.auth.token);
+  remove(id: string): void {
   }
 }
