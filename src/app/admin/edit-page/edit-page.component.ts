eedcc5aa0c735e5cc6e1b93d20635e61a136a29c
mfisher <matthewfisher1411@gmail.com>
2020-11-10 11:13:52 +0500

    added markup to edit-page component

--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {ActivatedRoute, Params} from '@angular/router';
+import {PostsService} from '../../shared/posts.service';
+import {switchMap} from 'rxjs/operators';
+import {Post} from '../../shared/interfaces';
 
 @Component({
   selector: 'app-edit-page',
@@ -6,10 +11,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./edit-page.component.scss']
 })
 export class EditPageComponent implements OnInit {
-
-  constructor() { }
+  form: FormGroup;
+  post: Post;
+  constructor(
+    private route: ActivatedRoute,
+    private postsService: PostsService
+  ) { }
 
   ngOnInit(): void {
+    this.route.params.pipe(
+      switchMap((params: Params) => {
+        return this.postsService.getById(params.id);
+      })
+    ).subscribe((post: Post) => {
+      this.post = post;
+      this.form = new FormGroup({
+        title: new FormControl(post.title, Validators.required),
+        text: new FormControl(post.text, Validators.required),
+      });
+    });
   }
 
+  submit(): void {
+    if (this.form.invalid) {
+      return;
+    }
+  }
 }
