ec21fdb8bf8c1c5fa35b4472d897bafb9350aa8c
mfisher <matthewfisher1411@gmail.com>
2020-11-10 09:17:35 +0500

    added remove post function to posts service

--- src/app/admin/dashboard-page/dashboard-page.component.ts
+++ src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -11,6 +11,7 @@ import {Subscription} from 'rxjs';
 export class DashboardPageComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   pSub: Subscription;
+  dSub: Subscription;
   searchStr = '';
   constructor(
     private postsService: PostsService
@@ -25,8 +26,14 @@ export class DashboardPageComponent implements OnInit, OnDestroy {
     if (this.pSub) {
       this.pSub.unsubscribe();
     }
+    if (this.dSub) {
+      this.dSub.unsubscribe();
+    }
   }
 
   remove(id: string): void {
+    this.dSub = this.postsService.remove(id).subscribe(() => {
+      this.posts = this.posts.filter(post => post.id !== id);
+    });
   }
 }
